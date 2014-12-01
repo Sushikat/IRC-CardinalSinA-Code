d13ffa163b01476ac4b8879cb3cdd754a49af954
Sushikat <ikatbuschel@comcast.net>
2014-11-30 19:23:01 -0800

    Added generic auto and tele-op code for tank drive.

--- SpiritCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ SpiritCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -11,7 +11,11 @@ package edu.wpi.first.wpilibj.templates;
  *  Spirit Group's Autonomous code for Cardinal Sin A
  */
 
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,15 +28,30 @@ public class RobotTemplate extends SimpleRobot {
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
+    RobotDrive chassis = new RobotDrive(1, 2);
+    Joystick leftStick = new Joystick(1);
+    Joystick rightStick = new Joystick(2);
+    Gyro gyro = new Gyro(1);
+    
+    double Kp = 0.03;
+    
     public void autonomous() {
-        
+        while(isAutonomous() && isEnabled()){
+            double angle = gyro.getAngle();
+            // Will switch these later
+            chassis.drive(-1.0, -angle*Kp);
+        }
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
+        chassis.setSafetyEnabled(true);
+        while(isOperatorControl() && isEnabled()){
+            chassis.tankDrive(leftStick, rightStick);
+            Timer.delay(0.01);
+        }
     }
     
     /**
