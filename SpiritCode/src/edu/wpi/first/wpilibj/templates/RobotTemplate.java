512aee0a3c00ca43b83bdcff45d9f95a0adf8bd4
Sushikat <ikatbuschel@comcast.net>
2014-12-01 22:48:20 -0800

    updated code in auto. still don't know if it works yet

--- SpiritCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ SpiritCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -25,46 +25,40 @@ import edu.wpi.first.wpilibj.Timer;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
-    /**
-     * This function is called once each time the robot enters autonomous mode.
-     */
     RobotDrive chassis = new RobotDrive(1, 2);
     Joystick leftStick = new Joystick(1);
     Joystick rightStick = new Joystick(2);
     Gyro gyro = new Gyro(1);
-    
-    double Kp = 0.03;
-    
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     * There are lots of while loops and it might look complicated,
+     * but you are repeating something several times, so it makes sense.
+     */
     public void autonomous() {
-        double angle = gyro.getAngle();
-        while(isAutonomous() && isEnabled()){
-            angle = gyro.getAngle();
-            // Will switch these later
-            chassis.drive(-2.0, -angle*Kp);
+        final double Kp = 0.03; //Proportionality Constant for turning (Google it)
+        int autoCount = 0; // Seconds counter
+        double angle = gyro.getAngle(); //The angle from home as measured by the gyro
+        int turns = 0; // The number of turns the robot has made (needs to make 4 i think)
+        while(turns < 3){ // Put in while loop to count number of times to turn
+            autoCount = 0;//Reset secs counter
+            while(autoCount < 3){ //Drive straight for 3 secs
+                while(isEnabled() && isAutonomous()){
+                    // Will switch these later
+                    chassis.drive(-.5, -angle*Kp);
+                    angle = gyro.getAngle(); //Fetch the angle for driving straight
+                }
+                Timer.delay(1.0); // Let one second of doing ^that pass
+                autoCount++; // Increment seconds counter
+            }
+            angle = gyro.getAngle() - 90; // Turn 90 (not sure if left or right)
+            turns++; // Increment turn count
         }
-        Timer.delay(2.0);
-        angle = gyro.getAngle() - 90.0;
-        while(isAutonomous() && isEnabled()){
-            angle = gyro.getAngle();
-            chassis.drive(-2.0, -angle * Kp);
-        }
-        Timer.delay(2.0);
-        angle = gyro.getAngle() -90.0;
-        while(isAutonomous() && isEnabled()){
-            angle = gyro.getAngle();
-            chassis.drive(-2.0, -angle * Kp);
-        }
-        Timer.delay(2.0);
-        angle = gyro.getAngle() -90.0;
-        while(isAutonomous() && isEnabled()){
-            angle = gyro.getAngle();
-            chassis.drive(-2.0, -angle * Kp);
-        }
-        
+        chassis.drive(0.0, 0.0); // Stop at the end
     }
 
     /**
      * This function is called once each time the robot enters operator control.
+     * This is filler I pulled off the FRC website
      */
     public void operatorControl() {
         chassis.setSafetyEnabled(true);
@@ -76,8 +70,9 @@ public class RobotTemplate extends SimpleRobot {
     
     /**
      * This function is called once each time the robot enters test mode.
+     * Can go??? Test and see!
      */
     public void test() {
-    
+        chassis.drive(-0.5, 0.0);
     }
 }
